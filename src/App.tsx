fa839a048a1f77cc9dbda0eaa7828e7a1d83775f
Shishir <shishir3324kumar@gmail.com>
2024-04-15 20:06:36 +0530

    wagmi blockchain project started

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+   <section className="pt-5">
+    <div className="container">
+      <h2 className="text-3xl mb-4">Wagmi Blockchain</h2>
+    </div>
+   </section>
+  )
+}
+
+export default App
