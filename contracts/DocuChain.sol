ba21f05dceed10e7803882f255e61fdef5c7b8ea
Shishir <shishir3324kumar@gmail.com>
2024-04-26 18:06:35 +0530

    created contract and written basic tests

---
+++ contracts/DocuChain.sol
@@ -0,0 +1,244 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DocuChain {
+    struct Student {
+        address studentAddress;
+        string fullName;
+        string email;
+        string gender;
+        // string dob;
+        // string qualification;
+        // string degree;
+        // string university;
+        // string startDate;
+        // string endDate;
+        // string gpa;
+        // string percentage;
+    }
+
+    struct Verifier {
+        address verifierAddress;
+        string name;
+        string email;
+        string instituteType;
+        string location;
+        string logoCid;
+    }
+
+    struct Document {
+        string cid;
+        string docName;
+        string docType;
+        address verifierAddress;
+        address studentAddress;
+        VerificationStatus status;
+    }
+
+    enum VerificationStatus {
+        Pending,
+        Verified,
+        Rejected
+    }
+
+    mapping(address => Student) students;
+    mapping(string => Document) documents;
+    mapping(address => Verifier) verifiers;
+
+    string[] public documentHashes;
+
+    // Functions to handle Documents in blockchain
+    /* 
+        1) Check if student details are added
+        2) Check if document is already available
+        3) Adding the document
+    */
+
+    function addDocumentDetails(
+        string memory _cid,
+        string memory _name,
+        string memory _docType,
+        address _verifierAddress,
+        address _studentAddress
+    ) public {
+        require(
+            students[msg.sender].studentAddress != address(0),
+            "Student details not added"
+        );
+
+        require(!isDocumentExists(_cid), "Document already added");
+
+        documentHashes.push(_cid);
+
+        documents[_cid] = Document(
+            _cid,
+            _name,
+            _docType,
+            _verifierAddress,
+            _studentAddress,
+            VerificationStatus.Pending
+        );
+    }
+
+    function getDocumentsForVerifier() public view returns (Document[] memory) {
+        Document[] memory verifierDocuments = new Document[](
+            documentHashes.length
+        );
+
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < documentHashes.length; i++) {
+            string memory hash = documentHashes[i];
+            Document memory doc = documents[hash];
+
+            if (doc.verifierAddress == msg.sender) {
+                verifierDocuments[count] = doc;
+                count++;
+            }
+        }
+
+        assembly {
+            mstore(verifierDocuments, count)
+        }
+
+        return verifierDocuments;
+    }
+
+    function getDocumentsForStudent() public view returns (Document[] memory) {
+        Document[] memory studentDocuments = new Document[](
+            documentHashes.length
+        );
+
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < documentHashes.length; i++) {
+            string memory hash = documentHashes[i];
+            Document memory doc = documents[hash];
+
+            if (doc.studentAddress == msg.sender) {
+                studentDocuments[count] = doc;
+                count++;
+            }
+        }
+
+        assembly {
+            mstore(studentDocuments, count)
+        }
+
+        return studentDocuments;
+    }
+
+    function getDocumentDetails(
+        string memory _cid
+    ) external view returns (Document memory) {
+        require(isDocumentExists(_cid), "Document not found");
+        return documents[_cid];
+    }
+
+    function verifyDocument(
+        string memory _cid,
+        VerificationStatus _status
+    ) public {
+        require(isDocumentExists(_cid), "Document not found");
+        require(isVerifierExists(msg.sender), "Verifier not found");
+        require(
+            documents[_cid].verifierAddress == msg.sender,
+            "Verifier not authorized"
+        );
+
+        documents[_cid].status = _status;
+    }
+
+    function deleteDocument(string memory _cid) public {
+        require(isDocumentExists(_cid), "Document not found");
+        require(isStudentExists(), "Student not found");
+        require(
+            documents[_cid].studentAddress == msg.sender,
+            "Student not authorized"
+        );
+
+        delete documents[_cid];
+    }
+
+    function isDocumentExists(string memory _cid) public view returns (bool) {
+        return bytes(documents[_cid].cid).length != 0;
+    }
+
+    // Functions for adding Student Details into the blockchain.
+
+    function addStudentDetails(
+        string memory _fullName,
+        string memory _email,
+        string memory _gender
+    ) public {
+        require(
+            students[msg.sender].studentAddress == address(0),
+            "Student Address already added"
+        );
+
+        students[msg.sender] = Student(msg.sender, _fullName, _email, _gender);
+    }
+
+    function isStudentExists() public view returns (bool) {
+        return students[msg.sender].studentAddress != address(0);
+    }
+
+    function getStudentDetails() external view returns (Student memory) {
+        return students[msg.sender];
+    }
+
+    function deleteStudentDetails() public {
+        require(
+            students[msg.sender].studentAddress != address(0),
+            "Student Details already deleted"
+        );
+        delete students[msg.sender];
+    }
+
+    /* 
+        Adding verifier address
+    */
+
+    function isVerifierExists(address _address) public view returns (bool) {
+        return verifiers[_address].verifierAddress != address(0);
+    }
+
+    function addVerifierDetails(
+        string memory _name,
+        string memory _email,
+        string memory _instituteType,
+        string memory _location,
+        string memory _logoCid
+    ) public {
+        require(!isVerifierExists(msg.sender), "Verifier already exists");
+
+        verifiers[msg.sender] = Verifier(
+            msg.sender,
+            _name,
+            _email,
+            _instituteType,
+            _location,
+            _logoCid
+        );
+    }
+
+    function deleteVerifier() public {
+        require(isVerifierExists(msg.sender), "Verifier does not exists");
+
+        delete verifiers[msg.sender];
+    }
+
+    function getVerifierDetails(
+        address _address
+    ) external view returns (Verifier memory) {
+        return verifiers[_address];
+    }
+
+    // function generateVerifierId(string memory _name, string memory _email)
+    //     external
+    //     pure
+    //     returns (bytes memory)
+    // {
+    //     return abi.encodePacked(_name, "_", _email);
+    // }
+}
---
+++ test/DocuChain.js
@@ -0,0 +1,111 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("DocuChain Functionality", function () {
+  let addresses = null;
+  let contract = null;
+
+  beforeEach(async function () {
+    let [owner, student, verifer] = await ethers.getSigners();
+    addresses = [owner, student, verifer];
+    contract = await ethers.deployContract("DocuChain", []);
+  });
+
+  it("check existence of student details in the contract", async function () {
+    const [owner, student, verifier] = addresses;
+
+    const sContract = await contract.connect(student);
+
+    const response1 = await sContract.isStudentExists();
+    await contract
+      .connect(student)
+      .addStudentDetails("John Doe", "john@gmail.com", "male");
+    const response2 = await sContract.isStudentExists();
+
+    expect(response1).to.equal(false);
+    expect(response2).to.equal(true);
+
+    const [sAddress, name, email] = await sContract.getStudentDetails();
+    console.log("Details of the student: ", sAddress, name, email);
+  });
+
+  it("check existence of verifier details in the contract", async function () {
+    const [owner, student, verifier] = addresses;
+
+    const vContract = await contract.connect(verifier);
+
+    const response1 = await vContract.isVerifierExists(verifier.address);
+    await vContract.addVerifierDetails(
+      "Saint Joseph School",
+      "example@co.in",
+      "school",
+      "Bhagalpur",
+      "random-cid"
+    );
+    const response2 = await vContract.isVerifierExists(verifier.address);
+
+    expect(response1).to.equal(false);
+    expect(response2).to.equal(true);
+
+    const [address, name, email] = await vContract.getVerifierDetails(
+      verifier.address
+    );
+    console.log("Details of the student: ", address, name, email);
+  });
+
+  it("adding document to the blockchain", async function () {
+    const [owner, student, verifier] = addresses;
+
+    const sContract = await contract.connect(student);
+    const vContract = await contract.connect(verifier);
+
+    await sContract.addStudentDetails("John Doe", "john@gmail.com", "male");
+
+    await sContract.addDocumentDetails(
+      "c14850f4d6da0e87622122d35c1bd379",
+      "Merit Certificate",
+      "certificate",
+      verifier.address,
+      student.address
+    );
+
+    await sContract.addDocumentDetails(
+        "6baa6e2decc2df28b5e0ffe627febbe4",
+        "Domicile Certificate",
+        "certificate",
+        verifier.address,
+        student.address
+      );
+
+    // const [docId, docName, docType, vAddress, sAddress] =
+    //   await sContract.getDocumentDetails("c14850f4d6da0e87622122d35c1bd379");
+
+    // expect(docId).to.not.equal("");
+
+    // console.log(await sContract.documentHashes(1))
+
+    const documents = await vContract.getDocumentsForStudent();
+
+    documents.forEach(async (doc) => {
+        const [docId, docName, docType, vAddress, sAddress] = doc;
+        console.log(
+            "Details of the document: ",
+            docId,
+            docName,
+            docType,
+            vAddress,
+            sAddress
+        );
+    })
+
+    // console.log(
+    //   "Details of the document: ",
+    //   docId,
+    //   docName,
+    //   docType,
+    //   vAddress,
+    //   sAddress
+    // );
+  });
+
+});
